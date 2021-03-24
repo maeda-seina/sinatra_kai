65fab209aadb16ecf4e14faaa1f3b54cdb0210b2
maeda-seina <ayrton19601027@gmail.com>
2021-03-24 11:11:15 +0900

    rubocopを通した

--- main.rb
+++ main.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'sinatra'
 require 'sinatra/reloader'
 require 'erb'
@@ -5,58 +7,58 @@ require 'securerandom'
 require 'json'
 
 get '/' do
-    files = Dir.glob("memos/*").sort_by { |file| File.mtime(file) }
-    @memos = files.map { |file| JSON.parse(File.read(file)) }
-    erb :top
+  files = Dir.glob('memos/*').sort_by { |file| File.mtime(file) }
+  @memos = files.map { |file| JSON.parse(File.read(file)) }
+  erb :top
 end
 
 get '/memos/new' do
-    erb :new
+  erb :new
 end
-  
+
 post '/memos' do
-    @title = params[:title]
-    @body = params[:body]
-    hash = {"id" => SecureRandom.uuid,"title" => @title,"body"=> @body}
-    File.open("memos/#{hash["id"]}.json", "w") do |file|
-        file.puts JSON.pretty_generate(hash)
-    end
-    redirect "/memos/show/#{hash["id"]}"
+  @title = params[:title]
+  @body = params[:body]
+  hash = { 'id' => SecureRandom.uuid, 'title' => @title, 'body' => @body }
+  File.open("memos/#{hash['id']}.json", 'w') do |file|
+    file.puts JSON.pretty_generate(hash)
+  end
+  redirect "/memos/show/#{hash['id']}"
 end
 
 get '/memos/show/:id' do
-    @title = params[:title]
-    @body = params[:body]
-    @memo = JSON.parse(File.read("memos/#{params[:id]}.json"), symbolize_names: true)
-    erb :show
+  @title = params[:title]
+  @body = params[:body]
+  @memo = JSON.parse(File.read("memos/#{params[:id]}.json"), symbolize_names: true)
+  erb :show
 end
 
 get '/memos/:id/edit' do
-    @title = params[:title]
-    @body = params[:body]
-    @memo = JSON.parse(File.read("memos/#{params[:id]}.json"), symbolize_names: true)
-    erb :edit
+  @title = params[:title]
+  @body = params[:body]
+  @memo = JSON.parse(File.read("memos/#{params[:id]}.json"), symbolize_names: true)
+  erb :edit
 end
 
 enable :method_override
 
 patch '/memos/:id' do
-    @title = params[:title]
-    @body = params[:body]
-    memo = { "id" => params[:id], "title" => @title, "body" => @body }
-    File.open("memos/#{params[:id]}.json", 'w') do |io|
-        io.puts(JSON.pretty_generate(memo))
-    end
-    redirect "/memos/show/#{params[:id]}"
+  @title = params[:title]
+  @body = params[:body]
+  memo = { 'id' => params[:id], 'title' => @title, 'body' => @body }
+  File.open("memos/#{params[:id]}.json", 'w') do |io|
+    io.puts(JSON.pretty_generate(memo))
+  end
+  redirect "/memos/show/#{params[:id]}"
 end
 
 delete '/memos/:id' do
-    File.delete("memos/#{params[:id]}.json")
-    redirect "/"
+  File.delete("memos/#{params[:id]}.json")
+  redirect '/'
 end
 
 helpers do
-    def h(text)
-        Rack::Utils.escape_html(text)
-    end
+  def h(text)
+    Rack::Utils.escape_html(text)
+  end
 end
